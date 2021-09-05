db1c7b0daf3b53fa280eb9e88239250752d8b2a8
unknown <cristiano_aguilar19@hotmail.com>
2021-09-04 23:07:38 -0700

    added the account creation date to users profile page

--- app/src/main/java/com/example/coinfolio/Profile_Screen.java
+++ app/src/main/java/com/example/coinfolio/Profile_Screen.java
@@ -9,22 +9,30 @@ import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class Profile_Screen extends AppCompatActivity {
-    private TextView Profile_email, Profile_name;
+    private TextView Profile_email, Profile_name, Profile_Date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile__screen);
         Profile_email = findViewById(R.id.profile_email);
         Profile_name = findViewById(R.id.profile_name);
+        Profile_Date = findViewById(R.id.profile_creation_date);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             // Name, email address, and profile photo Url
+            long creationDate = user.getMetadata().getCreationTimestamp();
             String name = user.getDisplayName();
             String email = user.getEmail();
 
+            DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+
             Profile_email.setText(email);
             Profile_name.setText(name);
+            Profile_Date.setText(dateFormat.format(creationDate));
         }
     }
 }
